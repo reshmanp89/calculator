c236fb78281a13a9120e24ee2980bc396ffa96dd
reshma <npreshmanp@gmail.com>
2023-02-17 16:52:53 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,16 @@
+function displayNum(num)
+{
+    d1.value+=num
+}
+function evaluateExpr()
+{
+    d1.value=eval(d1.value)
+}
+function allClear()
+{
+    d1.value=" "
+}
+function backSpace()
+{
+    d1.value=d1.value.slice(0,-1)
+}
